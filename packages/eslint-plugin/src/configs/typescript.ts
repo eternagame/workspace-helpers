88d9c9c6b896c25033eeb061c9d3913c72ac9635
Jonathan Romano <jonathan@luxaritas.com>
2022-08-02 16:05:04 -0400

    feat(nx,eslint-plugin): Enable linting for config files

--- packages/eslint-plugin/src/configs/typescript.ts
+++ packages/eslint-plugin/src/configs/typescript.ts
@@ -3,6 +3,7 @@ export default {
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
+      excludedFiles: '**/vite.config.ts',
       extends: [
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
@@ -12,6 +13,31 @@ export default {
         require.resolve('./base/javascript'),
         require.resolve('./base/typescript'),
       ],
+    },
+    {
+      // The vite configuration doesn't use our tsconfig, it uses vite's default settings,
+      // so we can't use rules that require a valid parserOptions.project
+      files: ['**/vite.config.ts'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'prettier',
+        require.resolve('./base/javascript'),
+        require.resolve('./base/typescript'),
+      ],
+      parserOptions: {
+        project: null,
+      },
+      rules: {
+        // We override this one
+        '@typescript-eslint/switch-exhaustiveness-check': 'off',
+        // airbnb-typescript override these
+        '@typescript-eslint/dot-notation': 'off',
+        '@typescript-eslint/no-implied-eval': 'off',
+        '@typescript-eslint/no-throw-literal': 'off',
+        '@typescript-eslint/return-await': 'off',
+      },
     },
   ],
 };
