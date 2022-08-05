5005ab5a8ad9ad0c5667bb662d23a1af266afaf6
Jonathan Romano <jonathan@luxaritas.com>
2022-08-05 17:03:35 -0400

    Docstrings

--- packages/nx/src/utils/json.ts
+++ packages/nx/src/utils/json.ts
@@ -1,3 +1,6 @@
+/**
+ * Given an object of an unknown type, check if it's an object that contains a particular key
+ */
 export function inOperator<K extends string, T>(
   k: K,
   o: T
@@ -5,6 +8,10 @@ export function inOperator<K extends string, T>(
   return o && typeof o === 'object' && k in o;
 }
 
+/**
+ * Given an item that may or may not be in a const array, determine whether it
+ * is indeed one of the items in that array, changing the type accordingly
+ */
 export function isArrayMember<T, U extends T>(
   x: T,
   arr: readonly U[]
