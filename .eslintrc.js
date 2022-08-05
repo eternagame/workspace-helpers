0b32aec17249f3583f3849552e5041776a4e02e0
Jonathan Romano <jonathan@luxaritas.com>
2022-08-05 19:12:09 -0400

    feat(nx,eslint-plugin): Enable linting for config files (#84)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
+const { join } = require('path');
+
 module.exports = {
   root: true,
+  ignorePatterns: fs
+    .readFileSync(join(__dirname, './.gitignore'), 'utf8')
+    .split('\n'),
   extends: [
     'plugin:@eternagame/typescript',
     'plugin:@eternagame/nx-typescript',
--- packages/eslint-plugin/vite.config.mjs
+++ packages/eslint-plugin/vite.config.mjs
@@ -1,3 +1,5 @@
+// Note that this file has to be mjs and not ts because we can't set type: module in our package.json,
+// as eslint relies on commonjs
 import getConfig from '@eternagame/vite';
 
 export default getConfig('lib', 'node');
