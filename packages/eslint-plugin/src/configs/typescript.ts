791d4765ca3a32472fdfa356ae138544bbab7c0a
Jonathan Romano <jonathan@luxaritas.com>
2022-08-08 14:29:39 -0400

    feat(nx,eslint-plugin,jest,tsconfig): Improve globs and use ts for all configs (#91)
    * Support module-specific file formats and use ts for all configs
    
    * Also support module file formats for lint-staged
    
    * Clean up additional globs
    
    * Improve globs in nx preset
    
    * Improve test globs
    
    * Add missing test extensions in nx preset
    
    * Expand valid test directories
    
    * Formatting fix
    
    * Simplify ts config linting

--- packages/eslint-plugin/src/configs/typescript.ts
+++ packages/eslint-plugin/src/configs/typescript.ts
@@ -1,10 +1,11 @@
-import { rules } from '@typescript-eslint/eslint-plugin';
+import { rules as tsEslintRules } from '@typescript-eslint/eslint-plugin';
+import noExtraneousDependencies from './base/extraneous-deps';
 
 export default {
   extends: require.resolve('./javascript'),
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.{ts,tsx,mts,cts}'],
       extends: [
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
@@ -16,26 +17,22 @@ export default {
       ],
     },
     {
-      // The vite configuration doesn't use our tsconfig, it uses vite's default settings,
-      // so we can't use rules that require a valid parserOptions.project
-      files: ['vite.config.ts'],
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'airbnb-base',
-        'airbnb-typescript/base',
-        'prettier',
-        require.resolve('./base/javascript'),
-        require.resolve('./base/typescript'),
-      ],
+      files: ['{jest,vite}.config.{ts,mts,cts}'],
       parserOptions: {
+        // Our configurations aren't tied to our tsconfig, so we can't use rules that require
+        // typechecking (nor indicate they should be tied to a tsconfig file)
         project: null,
       },
-      // Disable all rules that require type checking
-      rules: Object.fromEntries(
-        Object.entries(rules)
-          .filter(([, rule]) => rule.meta.docs?.requiresTypeChecking)
-          .map(([name]) => [`@typescript-eslint/${name}`, 'off'])
-      ),
+      rules: {
+        // Allow our configs to import dev dependencies
+        ...noExtraneousDependencies(true, false),
+        // Disable all rules that require type checking
+        ...Object.fromEntries(
+          Object.entries(tsEslintRules)
+            .filter(([, rule]) => rule.meta.docs?.requiresTypeChecking)
+            .map(([name]) => [`@typescript-eslint/${name}`, 'off'])
+        ),
+      },
     },
   ],
 };
