6892b1f46c726b0db5bff474f6770cc79eb1a4ab
Jonathan Romano <jonathan@luxaritas.com>
2022-08-16 16:55:40 -0400

    Merge ts-iso, ts-node, and ts-web into lib-ts generator

--- packages/nx-plugin/src/index.ts
+++ packages/nx-plugin/src/index.ts
@@ -3,6 +3,4 @@ export { default as webAppGenerator } from './generators/app-web';
 export { default as licenseGenerator } from './generators/license';
 export { default as packageGenerator } from './generators/package';
 export { default as presetGenerator } from './generators/preset';
-export { default as tsIsoGenerator } from './generators/ts-iso';
-export { default as tsNodeGenerator } from './generators/ts-node';
-export { default as tsWebGenerator } from './generators/ts-web';
+export { default as tsLibGenerator } from './generators/lib-ts';
