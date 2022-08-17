d5503503ad035497cf47efdd2a5adcbee2ed577b
Jonathan Romano <jonathan@luxaritas.com>
2022-08-17 13:32:16 -0400

    Set default parameters when running license generator

--- packages/nx-plugin/src/generators/license/index.ts
+++ packages/nx-plugin/src/generators/license/index.ts
@@ -7,7 +7,9 @@ import {
   type Tree,
 } from '@nrwl/devkit';
 import path from 'path';
-import { inOperator, isArrayMember } from '../../utils/json';
+import {
+  getValue, isArrayMember, isRecord, maybeInitObject,
+} from '../../utils/json';
 
 const licenseOptions = [
   'MIT',
@@ -62,51 +64,54 @@ function updateLicense(tree: Tree, root: string, options: Schema) {
 }
 
 export function updatePackageLicense(tree: Tree, projectRoot: string) {
-  const options = readNxJson().generators?.['@eternagame/nx-plugin:license']
-    || (readNxJson().generators?.['@eternagame/nx-plugin'] as Record<string, unknown>)?.[
-      'license'
-    ];
-  if (!options) {
-    logger.warn(
-      'Unable to get license generator options from nx.json - no license will be generated',
-    );
+  const nxJson = readNxJson();
+  const options = getValue(nxJson, null, 'generators', '@eternagame/nx-plugin:license')
+    || getValue(nxJson, null, 'generators', '@eternagame/nx-plugin', 'license');
+  if (!isRecord(options)) {
+    logger.info('Unable to get license generator defaults from nx.json - no license will be generated');
+    return;
   }
 
-  if (!inOperator('license', options)) {
-    throw new Error(
-      '"license" property not found in nx.json generator options',
-    );
-  }
-  const { license } = options;
-  if (typeof license !== 'string' || !isArrayMember(license, licenseOptions)) {
-    throw new Error(
-      `"license" property in nx.json generator options should be one of: [${licenseOptions.join(
-        ',',
-      )}]`,
-    );
+  const license = getValue(options, null, 'license');
+  if (!license) {
+    throw new Error('"license" property missing from @eternagame/nx-plugin:license generator defaults in nx.json');
+  } else if (typeof license !== 'string' || !isArrayMember(license, licenseOptions)) {
+    throw new Error(`"license" property for @eternagame/nx-plugin:license generator defaults in nx.json should be one of: [${licenseOptions.join(',')}]`);
   }
 
   let copyrightHolder = '';
   if (license !== 'None' && license !== 'Custom') {
-    if (!inOperator('copyrightHolder', options)) {
-      throw new Error(
-        '"copyrightHolder" property not found in nx.json generator options',
-      );
+    const defaultCopyrightHolder = getValue(options, null, 'copyrightHolder');
+
+    if (!defaultCopyrightHolder) {
+      throw new Error('"copyrightHolder" property missing from @eternagame/nx-plugin:license generator defaults in nx.json');
+    } else if (typeof defaultCopyrightHolder !== 'string') {
+      throw new Error('"copyrightHolder" property for @eternagame/nx-plugin:license generator defaults in nx.json should be a string');
     }
 
-    if (typeof options.copyrightHolder !== 'string') {
-      throw new Error(
-        '"copyrightHolder" property in nx.json generator options should be a string',
-      );
-    }
-
-    copyrightHolder = options.copyrightHolder;
+    copyrightHolder = defaultCopyrightHolder;
   }
 
   updateLicense(tree, projectRoot, { license, copyrightHolder });
 }
 
 export default function generate(tree: Tree, options: Schema) {
+  updateJson(tree, 'nx.json', (json: Record<string, unknown>) => {
+    const generators = maybeInitObject(json, 'generators');
+    if (!generators) throw new Error('nx.json generators property is invalid');
+    const pluginDefaults = getValue(generators, null, '@eternagame/nx-plugin');
+    const licenseDefaults = isRecord(pluginDefaults)
+      ? maybeInitObject(pluginDefaults, 'license')
+      : maybeInitObject(generators, '@eternagame/nx-plugin:license');
+    if (!licenseDefaults) {
+      throw new Error('Generator defaults for @eternagame/nx-plugin:license in nx.json is invalid');
+    }
+    licenseDefaults['license'] = options.license;
+    licenseDefaults['copyrightHolder'] = options.copyrightHolder;
+
+    return json;
+  });
+
   updateLicense(tree, '', options);
 
   for (const projectConfig of getProjects(tree).values()) {
--- packages/nx-plugin/src/utils/json.ts
+++ packages/nx-plugin/src/utils/json.ts
@@ -1,7 +1,12 @@
+type RecordKey = string | number | symbol;
+
 /**
- * Given an object of an unknown type, check if it's an object that contains a particular key
+ * Given a variable of an unknown type, check if it's an object that contains a particular key
+ *
+ * @param k The key to check for in o
+ * @param o The variable to check for k in
  */
-export function inOperator<K extends string, T>(
+export function inOperator<K extends RecordKey, T>(
   k: K,
   o: T,
 ): o is T & Record<K, unknown> {
@@ -9,12 +14,91 @@ export function inOperator<K extends string, T>(
 }
 
 /**
+ * Given a variable of an unknown type, check if it's an object that contains a particular key.
+ * If it does, return it. Otherwise, return a default value.
+ *
+ * @param o The variable to check for k in
+ * @param defaultValue The default value to return if k is not in o
+ * @param key The key to check for in o
+ */
+export function getValue(
+  o: unknown,
+  defaultValue: unknown,
+  key: string
+): unknown;
+/**
+ * Given a variable of an unknown type, check if it's an object that contains a particular key.
+ * If it does, return it. Otherwise, return a default value.
+ *
+ * @param o The variable to check for k in
+ * @param defaultValue The default value to return if k is not in o
+ * @param keys A chain of keys to check for in o (eg, (o, null, 'foo', 'bar') will check for
+ *  o.foo.bar)
+ */
+export function getValue(
+  o: unknown,
+  defaultValue: unknown,
+  ...keys: [RecordKey, ...RecordKey[]]
+): unknown;
+export function getValue(
+  o: unknown,
+  defaultValue: unknown,
+  ...keys: [RecordKey, ...RecordKey[]]
+): unknown {
+  let currVal = o;
+  for (const key of keys) {
+    if (!inOperator(key, currVal)) return defaultValue;
+    currVal = currVal[key];
+  }
+  return defaultValue;
+}
+
+/**
  * Given an item that may or may not be in a const array, determine whether it
  * is indeed one of the items in that array, changing the type accordingly
+ *
+ * @param x The value to check arr for
+ * @param arr The array to check for x in
  */
 export function isArrayMember<T, U extends T>(
   x: T,
   arr: readonly U[],
 ): x is typeof arr[number] {
   return arr.includes(x as U);
+}
+
+/**
+ * Determines whether the parameter is a string-keyed Record
+ *
+ * @param o The possibly-record to check
+ * @returns Whether or not o is a string-keyed  Record
+ */
+export function isRecord(o: unknown): o is Record<string, unknown> {
+  return typeof o === 'object' && o !== null && Object.keys(o).every((k) => typeof k === 'string');
+}
+
+/**
+ * Check if an object appears at a key of another object. If it does not, create an empty object
+ * at that key. Return either the existing or new object
+ *
+ * @param o The object to check for an object value
+ * @param k The key where the object value should be
+ * @returns The existing object if it already exists, the new object if it doesn't, or null if
+ *  the key exists but the value is not an object
+ */
+export function maybeInitObject<K extends string>(
+  o: Record<string, unknown>,
+  k: K,
+): Record<string, unknown> | null {
+  if (k in o) {
+    const value = o[k];
+    if (isRecord(value)) {
+      return value;
+    }
+    return null;
+  }
+  const newObject = {};
+  // eslint-disable-next-line no-param-reassign
+  o[k] = newObject;
+  return newObject;
 }
