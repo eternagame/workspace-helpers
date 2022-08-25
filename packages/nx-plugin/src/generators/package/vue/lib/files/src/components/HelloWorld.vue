d5922327c3d0b11954b866ead8af6c12d39f442f
Jonathan Romano <jonathan@luxaritas.com>
2022-08-25 18:20:08 -0400

    Add vue-lib generator

---
+++ packages/nx-plugin/src/generators/package/vue/lib/files/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{message}}</h1>
+  <button @click="emit('buttonClicked')">Click Me!</button>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    message: {
+      type: String,
+      required: true
+    }
+  });
+
+  const emit = defineEmits<{
+    (e: 'buttonClicked'): void
+  }>();
+</script>
---
+++ packages/nx-plugin/src/generators/package/vue/lib/index.ts
@@ -0,0 +1,86 @@
+import * as path from 'path';
+import {
+  generateFiles,
+  getWorkspaceLayout,
+  joinPathFragments,
+  names,
+  updateJson,
+  type Tree,
+} from '@nrwl/devkit';
+import generateWebLib from '../../web/lib';
+import { installDependencies, installDevDependencies } from '../../../../utils/dependencies';
+
+interface Schema {
+  name: string;
+  description: string;
+  directory: string;
+}
+
+interface NormalizedSchema extends Schema {
+  projectRoot: string;
+}
+
+function normalizeOptions(tree: Tree, options: Schema): NormalizedSchema {
+  const name = names(options.name).fileName;
+  const { libsDir } = getWorkspaceLayout(tree);
+
+  const projectDirectory = options.directory
+    ? `${names(options.directory).fileName}/${name}`
+    : name;
+
+  const projectRoot = joinPathFragments(libsDir, projectDirectory);
+
+  return {
+    ...options,
+    projectRoot,
+  };
+}
+
+function addFiles(tree: Tree, options: NormalizedSchema) {
+  const templateOptions = {
+    ...options,
+    tmpl: '',
+  };
+  generateFiles(
+    tree,
+    path.join(__dirname, 'files'),
+    options.projectRoot,
+    templateOptions,
+  );
+}
+
+export default async function generate(tree: Tree, options: Schema) {
+  const normalizedOptions = normalizeOptions(tree, options);
+
+  const finalizeWebLib = await generateWebLib(tree, normalizedOptions);
+
+  addFiles(tree, normalizedOptions);
+
+  // Update build tsconfig
+  updateJson(
+    tree,
+    joinPathFragments(normalizedOptions.projectRoot, 'tsconfig.build.json'),
+    (json: { 'include': string[] }) => {
+      // eslint-disable-next-line no-param-reassign
+      json.include = [...json.include, 'src/**/*.vue'];
+      return json;
+    },
+  );
+
+  // Update package.json
+  updateJson(
+    tree,
+    joinPathFragments(normalizedOptions.projectRoot, 'package.json'),
+    (json: ({ scripts: { build: string } })) => {
+      // eslint-disable-next-line no-param-reassign
+      json.scripts.build = `vue-tsc --noEmit --pretty -p tsconfig.build.json && ${json.scripts.build}`;
+      return json;
+    },
+  );
+
+  return async () => {
+    await finalizeWebLib();
+    installDependencies(tree, ['vue'], normalizedOptions.projectRoot);
+    installDevDependencies(tree, ['vue-tsc']);
+  };
+}
